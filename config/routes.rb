2b1886423ebcb6bcb4e8f0b59955ee29427d6623
Andreas Van Assche <andreasava@hotmail.com>
2020-05-18 10:42:20 +0200

    mockup

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     get 'consultancy', to: "pages#solutions"
     get 'cookies', to: "pages#cookies"
     get 'online', to: "pages#online"
+    get 'mockup', to: "pages#mock-up"
 
     resources :products, only: [ :create, :new, :show, :update, :edit, :destroy, :index ]
     resources :messages, only: [ :create ]
