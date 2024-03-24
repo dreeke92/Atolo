019ccbb7f72f11873dea351454a68c5746077fc8
vincent simon <vincent.simon@gadz.org>
2024-03-24 16:25:08 +0100

    ok summer_class

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,8 +29,8 @@ class PagesController < ApplicationController
   def summer_class
     if Rails.env.production?
     # @products = Product.take(3)
-    @products_1 = Product.where("id in (221, 222)").order(:id)
-    @products_2 = Product.where("id in (223, 224)").order(:id)
+    @products_1 = Product.where("id in (259, 260)").order(:id)
+    @products_2 = Product.where("id in (261, 262)").order(:id)
     else
       @products_1 = Product.take(2)
       @products_2 = Product.take(2)
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
-  validates :name_en, presence: true, uniqueness: true
+  validates :name_en, presence: true
   validates :group, presence: true
   # validates :description_en, presence: true
   # validates :image, presence: true, uniqueness: true
